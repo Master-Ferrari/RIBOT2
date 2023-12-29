809859dd056852c6942dd19e021093964cecf8b8
Master-Ferrari <128731554+Master-Ferrari@users.noreply.github.com>
2023-12-29 08:08:39 +0300

    sqlite module added

---
+++ source/lib/sqlite.ts
@@ -0,0 +1,172 @@
+import sqlite3 from 'sqlite3';
+
+interface JSONValue {
+    [key: string]: any;
+}
+
+class Database {
+    private dbPath: string;
+    private db: sqlite3.Database | null = null;
+
+    constructor(dbPath: string) {
+        this.dbPath = dbPath;
+    }
+
+    init(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.db = new sqlite3.Database(this.dbPath, (err: any) => {
+                if (err) {
+                    console.error('Could not connect to database', err);
+                    reject(err);
+                } else {
+                    pppprint('Connected to database');
+                    this.db!.run(`CREATE TABLE IF NOT EXISTS objects (
+                        key TEXT PRIMARY KEY,
+                        value TEXT
+                    );`, (err: any) => {
+                        if (err) {
+                            console.error('Could not create table', err);
+                            reject(err);
+                        } else {
+                            pppprint('Database initialized');
+                            resolve();
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    setJSON(tableName: string, key: string, object: JSONValue): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.db!.run(
+                `CREATE TABLE IF NOT EXISTS ${tableName} (
+                    key TEXT PRIMARY KEY,
+                    value TEXT
+                );`, (err: any) => {
+                if (err) {
+                    console.error(`Could not create table ${tableName}`, err);
+                    reject(err);
+                } else {
+                    const jsonString = JSON.stringify(object);
+                    this.db!.run(`INSERT OR REPLACE INTO ${tableName} (key, value) VALUES (?, ?);`, [key, jsonString], (err: any) => {
+                        if (err) {
+                            console.error('Could not add or update object', err);
+                            reject(err);
+                        } else {
+                            pppprint('Object added or updated');
+                            resolve();
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    getJSON(tableName: string, key: string): Promise<JSONValue | null> {
+        return new Promise((resolve, reject) => {
+            this.db!.get(`PRAGMA table_info(${tableName});`, (err: any, row: any) => {
+                if (err) {
+                    console.error('Could not retrieve table info', err);
+                    reject(err);
+                } else {
+                    if (!row) {
+                        resolve(null); // Возвращаем null, если таблицы нет
+                    } else {
+                        this.db!.get(`SELECT value FROM ${tableName} WHERE key = ?;`, [key], (err: any, row: { value: string; }) => {
+                            if (err) {
+                                console.error('Could not retrieve object', err);
+                                reject(err);
+                            } else {
+                                resolve(row ? JSON.parse(row.value) : null);
+                            }
+                        });
+                    }
+                }
+            });
+        });
+    }
+
+    close(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.db) {
+                this.db.close((err: any) => {
+                    if (err) {
+                        console.error('Could not close database', err);
+                        reject(err);
+                    } else {
+                        pppprint('Database closed');
+                        resolve();
+                    }
+                });
+            }
+        });
+    }
+    getAllJSON(tableName: string): Promise<JSONValue[] | null> {
+        return new Promise((resolve, reject) => {
+            this.db!.get(`SELECT name FROM sqlite_master WHERE type='table' AND name=?;`, [tableName], (err: any, row: any) => {
+                if (err) {
+                    console.error(`Could not check table existence`, err);
+                    reject(err);
+                } else if (!row) {
+                    resolve(null); // Если таблицы нет, верните null
+                } else {
+                    this.db!.all(`SELECT value FROM ${tableName};`, (err: any, rows: any[]) => {
+                        if (err) {
+                            console.error(`Could not retrieve objects from table ${tableName}`, err);
+                            reject(err);
+                        } else {
+                            const allObjects = rows.map((row: { value: string; }) => JSON.parse(row.value));
+                            resolve(allObjects);
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    deleteRecord(tableName: string, key: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.db!.run(`DELETE FROM ${tableName} WHERE key = ?;`, [key], (err: any) => {
+                if (err) {
+                    console.error('Could not delete object', err);
+                    reject(err);
+                } else {
+                    pppprint('Object deleted');
+                    resolve();
+                }
+            });
+        });
+    }
+
+    deleteTable(tableName: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.db!.run(`DROP TABLE IF EXISTS ${tableName};`, (err: any) => {
+                if (err) {
+                    console.error('Could not delete table', err);
+                    reject(err);
+                } else {
+                    pppprint('Table deleted');
+                    resolve();
+                }
+            });
+        });
+    }
+
+    static async interact(DBpath: string, callback: (db: Database) => Promise<void>): Promise<void> {
+        const db = new Database(DBpath);
+        await db.init();
+        await callback(db);
+        await db.close();
+    }
+    
+}
+
+
+
+
+export default Database;
+
+function pppprint(msg: string): void {
+    if (false) console.log(msg);
+}
